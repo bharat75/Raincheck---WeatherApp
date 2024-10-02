7c705bb07b8199ac9329f39424dca10d9be62db5
BharatShubham <bharatshubham75@gmail.com>
2024-10-02 11:49:57 +0530

    First Commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <nav>
+        <a>Location</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
